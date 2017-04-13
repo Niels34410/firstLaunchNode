51daea5b89e3c1f310569cf67578579445609038
Niels34410 <chaigneau.niels@gmail.com>
2017-04-13 12:32:25 +0200

    mise en place du framework express

--- server.js
+++ server.js
@@ -1,17 +1,33 @@
-var http = require('http');
+//var http = require('http');
+//
+//var ip = '127.0.0.1';
+//var port = 12107;
+//
+//var server = http.createServer(
+//    function (request,response){
+//	response.statusCode = 200;
+//	response.setHeader('Content-Type','text/plain');
+//	response.end('texte')
+//}
+//
+//);
+//
+//server.listen(port,ip,function(){
+//    console.log('txt')
+//});
+var express = require('express');
 
-var ip = '127.0.0.1';
-var port = 12107;
+var server = express();
+var routeToto = "/toto";
 
-var server = http.createServer(
-    function (request,response){
-	response.statusCode = 200;
-	response.setHeader('Content-Type','text/plain');
-	response.end('texte')
-}
-
-);
-
-server.listen(port,ip,function(){
-    console.log('txt')
+server.get("/",function(request,response){
+    response.sendFile(__dirname + "/views/index.html");
 });
+server.get(routeToto,function(request,response){
+    response.send("message venant de toto");
+});
+server.post("/toto",function(request,response){
+    response.send("message venant de toto en post");
+});
+
+server.listen(12107);
